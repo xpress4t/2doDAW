cb0893bf6619702213238e8b98ee5a5a3d495225
xpress4t <emmanuelcm2899@gmail.com>
2024-09-17 11:17:16 +0200

    Primera clase de PHP

---
+++ PHP/empezando.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<title>Empezando</title>
+</head>
+<body>
+	<?php 
+		echo "<p>Hola mundo</p>";
+		echo "<p>Hola mundo</p>";
+		echo "<p>La suma de 2 + 2 es igual a 7</p>"
+
+	?>
+
+</body>
+</html>
---
+++ PHP/holaMundo.php
@@ -0,0 +1,20 @@
+<?php
+// Mostrar un texto en pantalla
+echo "Hola Mundo";
+// Declarando variables
+$var = 1;
+$var2 = 2;
+$var3 = 3;
+// Imprimiendo la operación entre esas 3 variables
+print_r($var*$var2*$var3);
+// 
+echo "3ra linea impresa en pantalla";
+echo "<p>Hola mundo</p>";
+echo "<p>Hola mundo</p>";
+echo "<p>La suma de 2 + 2 es igual a 7</p>"
+
+// Se utiliza comillas simples cuando es un enlace
+
+// Pasar los elementos en bloque a través de css
+
+?>
