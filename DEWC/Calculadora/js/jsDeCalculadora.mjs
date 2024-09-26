5f5b64c8ad2e436c7b0a65b2c21a1363e31762db
Emmanuel <emmanuelcm2899@gmail.com>
2024-09-26 14:37:49 +0200

    Ejercicios en clase

---
+++ DEWC/Calculadora/js/jsDeCalculadora.mjs
@@ -0,0 +1,39 @@
+var botones = document.querySelectorAll("button")
+var inp = document.querySelector("#resultado")
+
+// input --> tienen value --> value para todo lo que no sea texto
+// textos --> innerText --> para todo lo que no sea input
+
+let operacionActual = "";
+let operador = "";
+
+function agregarTextoInput(e){
+    let valorBoton = e.target.innerText;
+
+    if (e.target.classList.contains("numeros")) {
+        operacionActual += valorBoton;
+        inp.value = operacionActual;
+    }
+
+    if (e.target.classList.contains("operación")) {
+        if (operacionActual === "") return;
+            operador = valorBoton;
+            operacionActual += " " + valorBoton + " ";
+            inp.value = operacionActual;
+    }
+
+    if (e.target.classList.contains("igual")) {
+        let resultado = eval(operacionActual.replace("x", "*").replace("%", "/100"));
+        inp.value = resultado;
+        operacionActual = resultado.toString();  
+    }
+
+    if (e.target.classList.contains("ponerEnCero")) {
+        operacionActual = "";
+        inp.value = "";
+    }
+}
+
+for(var i=0;i<botones.length;i++){
+    botones[i].addEventListener("click",agregarTextoInput)
+}
---
+++ DEWC/Conversor de Temperaturas/js/logicaTemp.mjs
@@ -0,0 +1,6 @@
+// Celsius a Fahrenheit	T(°F) = [(9/5)T(°C)]+32 ‍
+// Farenheit a Celsius	T(°C) = (5/9)[T(°F)−32] ‍
+// Celsius a Kelvin	T(K) = T(°C)+273
+// Kelvin a Celsius	T(°C) = T(K)-273
+// Fahrenheit a Kelvin	T(K) = [(5/9)(T(°F)-32)]+273
+// Kelvin a Fahrenheit	T(°F) = [(9/5)(T(K)-273)+32]
