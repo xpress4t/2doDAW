23338ea86473f62a4100c5206946d9bfe1f9a69a
Emmanuel <emmanuelcm2899@gmail.com>
2024-09-27 10:26:03 +0200

    Ejercicios en clase

--- DEWC/Conversor de Temperaturas/js/logicaTemp.mjs
+++ DEWC/Conversor de Temperaturas/js/logicaTemp.mjs
@@ -4,3 +4,5 @@
 // Kelvin a Celsius	T(°C) = T(K)-273
 // Fahrenheit a Kelvin	T(K) = [(5/9)(T(°F)-32)]+273
 // Kelvin a Fahrenheit	T(°F) = [(9/5)(T(K)-273)+32]
+
+var input = document.querySelector("#textoIngresado");
