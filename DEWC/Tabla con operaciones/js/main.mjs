252f5b053eb636715f63bcfa7a3d0d19f2c4d8c4
xpress4t <emmanuelcm2899@gmail.com>
2024-09-30 13:40:00 +0200

    -

---
+++ DEWC/Tabla con operaciones/js/main.mjs
@@ -0,0 +1,6 @@
+let inputs = document.querySelectorAll
+let aleatorios = document.querySelectorAll("td.aleatorio");
+let resultados = document.querySelectorAll
+
+// Es un metodo de solucion ineficiente lo de abajo, debo hacerlo mejor
+// inputs[0] * aleatorios[0] = resultados [0]
