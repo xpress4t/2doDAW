4009dbe8cd078d49218dcef5caa8cda94e5ce6e5
Emmanuel <emmanuelcm2899@gmail.com>
2024-10-26 16:22:55 +0200

    pokedex

---
+++ DEWC/Pokedex/js/app.js
@@ -0,0 +1,30 @@
+var div = document.querySelector('.pokemon-image')
+imagenPokemon()
+var lista = document.querySelector('#lista-pokemones')
+
+function imagenPokemon(){
+    fetch("https://pokeapi.co/api/v2/pokemon/totodile")
+    .then(resultado => resultado.json())
+    .then(resultado => {
+        // Esto es para mostrar la imagen estática del pokemon
+        //div.innerHTML = "<img src="+resultado.sprites.front_default+">"
+        div.innerHTML = "<img src="+resultado.sprites.other.showdown.front_default+">"
+    })
+    .catch(error => {
+            console.log("Error al obtener imagen del Pokémon"+error)
+    })
+}
+// Muestra en pantalla la imagen del pokemon
+
+// Al hacer click que me aparezca la animación del pokémon
+div.addEventListener("click",event =>{
+    event.target.style.backgroundColor = "red"
+})
+
+function maousePorEncima(){
+    fetch()
+    .then(resultado => resultado.json())
+    .then(resultado => {
+        clase.innerHTML = ""
+    })
+}
