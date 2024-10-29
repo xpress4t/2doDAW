5b5c045af8f5657729f874660a33545670134400
xpress4t <emmanuelcm2899@gmail.com>
2024-10-29 14:23:39 +0100

    clase php

---
+++ PHP/clase29-10.php
@@ -0,0 +1,55 @@
+<?php
+    echo "<h1>PHP</h1>";
+
+    $nombre = "Emmanuel";
+    $apellido1 = "Cordova";
+    $apellido2 = "Muñoz";
+
+    echo "<p>Nombre: ".$nombre."</p>";
+    echo "<p>Primer apellido: ".$apellido1."</p>";
+    echo "<p>Segundo apellido: ".$apellido2."</p>";
+
+    echo "<h2>Concatenación de variables</h2>";
+    // Concatenando con comillas dobles
+    echo "<p> $nombre $apellido1 $apellido2 </p>";
+
+    // Concatenando con comillas simples y con puntos
+    echo '<p>'.$nombre.' '.$apellido1.' '.$apellido2.'</p>';
+
+    // Concatenando con comillas simples (aparece en pantalla el nombre de las variables "$nombre", etc)
+    echo '<p> $nombre $apellido1 $apellido2 </p>';
+
+    echo "<h2>Bucle WHILE</h2>";
+    // Bucle while 
+    $index = 0;
+    while($index <= 10){
+        echo "<p>El contador vale: $index </p>";
+        $index++;
+    }
+
+    echo "<h2>Bucle FOR</h2>";
+
+    for($i=0;$i<=5;$i++){
+        echo "<p>El contador vale: $i </p>";
+    }
+
+    echo "<h2>Estructura de control SWITCH</h2>";
+    $variable = 5;
+    switch($variable){
+        case 5:
+            echo "<p>El valor es 5</p>";
+            break;
+
+        case 6:
+            echo "<p>El valor es 6</p>";
+            break;
+
+        case 7:
+            echo "<p>El valor es 7</p>";
+            break;
+        default:
+            echo "<p>No se que valor tienes</p>";
+    }
+
+    // donde la entrada sea el dia de la semana y el dia sea 1 o el lunes
+?>
---
+++ PHP/diaDeLaSemana.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $numero = 1;
+
+        switch($numero){
+            case 1:
+                echo "Lunes";
+            break;
+            case 2:
+                echo "Martes";
+            break;
+            case 3:
+                echo "Miércoles";
+            break;
+            case 4:
+                echo "Jueves";
+            break;
+            case 5:
+                echo "Viernes";
+            break;
+            case 6:
+                echo "Sábado";
+            break;
+            case 7:
+                echo "Domingo";
+            break;
+        }
+
+        echo "<h2>Operaciones básicas</h2>";
+
+        $var1 = 6;
+        $var2 = 9;
+        $operador = "/";
+
+        if($operador == "+"){
+            echo "El resultado de la suma $var1 + $var2 es: ".$var1+$var2;
+        }else if($operador == "-"){
+            echo "El resultado de la resta $var1 - $var2 es: ".$var1-$var2;
+        }else if($operador == "*"){
+            echo "El resultado de la multiplicación $var1 * $var2 es: ".$var1*$var2;
+        }else if($operador == "/"){
+            echo "El resultado de la división $var1 / $var2 es: ".$var1/$var2;
+        }
+        
+    ?>
+
+    <?php
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
+
+        $altura = 5;
+
+        for ($m=1;$m<=$altura;$m++){
+            for($n=1;$n<=(2*$m-1);$n++){
+                echo "*";
+            }
+            echo "<br>";
+        }
+    ?>
+
+    <?php
+        echo "<br>";
+        echo "<br>";
+        echo "<br>";
+        $a = 8;
+        for($i=1;$i<=$a;$i++){
+            echo "    ";
+            for($j=1;$j<=(2*$i-1);$j++){
+                echo "*";
+            }
+            echo "<br>";
+        }
+    ?>
+</body>
+</html>
