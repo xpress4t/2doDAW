66f90f2c9c64996cb172cf22baadeca1c5994021
Emmanuel <emmanuelcm2899@gmail.com>
2024-10-29 02:24:22 +0100

    pokedex

--- DEWC/Pokedex al estilo Andres/js/app.js
+++ DEWC/Pokedex al estilo Andres/js/app.js
@@ -1,4 +1,16 @@
-var miTabla = document.getElementById('table-body');
+const searchInfoButton = document.getElementById("searchInfo")
+const searchAllButton = document.getElementById("searchAll")
+const searchAllSortedButton = document.getElementById("searchAllSorted")
+const miTabla = document.getElementById('table-body');
+
+searchInfoButton.addEventListener("click",event => {
+})
+
+searchAllButton.addEventListener("click",event => {
+})
+
+searchAllSortedButton.addEventListener("click",event => {
+})
 
 function obtenerPokemon(){
     fetch("https://pokeapi.co/api/v2/pokemon?limit=100")
