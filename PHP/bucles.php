8ec4856bd88b4f2d69a75ddba2048eba566086f6
Emmanuel <emmanuelcm2899@gmail.com>
2024-11-20 02:25:55 +0100

    -

--- PHP/bucles.php
+++ PHP/bucles.php
@@ -14,23 +14,17 @@
 
 <body>
     <?php
-    echo "<h1>Bucles</h2>";
-    echo "<h2>For</h2>";
-
     for ($vueltas = 0; $vueltas < 10; $vueltas++) {
         echo "<p>Esta es la vuelta número $vueltas</p>";
     }
 
-
-    echo "string";
-    echo "Nº aleatorio del 1 al 10";
     $numero = rand(1, 10);
     echo "<p>Tabla del $numero</p>";
 
     for ($i = 1; $i <= 10; $i++) {
         echo "<p>$numero x $i = " . ($numero * $i) . "</p>";
     }
-    ?>
+    ?> 
 
 
 </body>
