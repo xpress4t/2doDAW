418d09bc875a6cbfe81b2306bcfbb1a7489fe36f
xpress4t <emmanuelcm2899@gmail.com>
2025-01-13 09:43:46 +0100

    iniciando en react

---
+++ React/app.js
@@ -0,0 +1,5 @@
+var inputs = [];
+inputs.push(
+    {color: "azul", required: true, type: "text", label: "Nombre"},
+    {required: true, type: "text", label: "Apellido1"},
+);
---
+++ React/app1.js
@@ -0,0 +1,10 @@
+import React from "react";
+import {CustomImputs}
+
+export const CustomForm = ({inputs}) => {
+
+    return <form>
+        {inputs && inputs.map((input, index) => {
+            )}
+    </form>
+}
