30f76583440c34caa6f2bf65b135566dd6bdbb68
xpress4t <emmanuelcm2899@gmail.com>
2025-01-28 14:11:43 +0100

    -

--- PHP/Agenda/index.php
+++ PHP/Agenda/index.php
@@ -6,115 +6,102 @@
     <title>Document</title>
     <link rel="stylesheet" href="CSS/style.css">
 </head>
-<style>
-    img{
-        margin-right: .8em;
-        height: 150px;
-        height: 150px;
-        vertical-align: middle;/*Para poner el texto a la altura de la mitad de la imagen */
-    }
-    
-    .info-par, .info-impar{
-        background: #cdffff;
-
-    }
-</style>
 <body>
-        <header>
-            <a href="index.php"><img src="media/img/cabecera.jpg" alt=""></a>
-        </header>
-            <h1>Listado de contactos</h1>
-            <?php
-            // require("conexion_agenda.php"); // Importo el archivo conexion_agenda.php
-            // try{
-            //     $conexion=mysqli_connect($servidor,$usuario,$password,$bbdd);
-            //     if(!$conexion){
-            //         throw new Exception("No se pudo conectar al servidor: ".mysqli_connect_error());
-            //     }
-            //     if(!mysqli_select_db($conexion,$bbdd)){
-            //        throw new Exception("No se pudo encontrar la base de datos: " . mysqli_error($conexion));
-            //    }
-            //     mysqli_query($conexion, "SET NAMES 'UTF8'");
-            //     $consulta = "SELECT * FROM contactos ORDER BY nombre, apellidos"; //definimos la query
-            //     $resultado = mysqli_query($conexion,$consulta); //ejecutamos la query
-            //    if(!$resultado){
-            //         throw new Exception("Error al ejecutar la consulta: " . mysqli_error($conexion));
-            //     }
-            //     while($fila = mysqli_fetch_array($resultado)){
-            //         echo "<p>$fila[cod_contacto]</p>";
-            //         echo "<p>$fila[nombre]</p>";
-            //         echo "<p>$fila[apellidos]</p>";
-            //     }
-            // }catch(Exception $e){
-            //     echo "<p>error: ".mysqli_errno($conexion)."</p>";
-            //     echo "<p>Mensaje: ".mysqli_error($conexion)."</p>";
-            // }
-            /*error_reporting(0) desactiva la notificacion de errores*/
-            /*error_reporting(E_ALL ^ E_WARNING/E_NOTICE) desactivar todo excepto tal o/tal */
-            // error_reporting(0);
-            try{
-                require("conexion_agenda.php");
-               
-            }catch(Throwable $t){
-                echo "<p>----------------------------------------</p>";
-                echo "Mensaje: ".$t->getMessage();
-                echo "<p>----------------------------------------</p>";
-                exit();
+    <h1>Listado de contactos</h1>
+    <?php
+    // require("conexion_agenda.php"); // Importo el archivo conexion_agenda.php
+    // try{
+    //     $conexion=mysqli_connect($servidor,$usuario,$password,$bbdd);
+    //     if(!$conexion){
+    //         throw new Exception("No se pudo conectar al servidor: ".mysqli_connect_error());
+    //     }
+    //     if(!mysqli_select_db($conexion,$bbdd)){
+    //        throw new Exception("No se pudo encontrar la base de datos: " . mysqli_error($conexion));
+    //    }
+    //     mysqli_query($conexion, "SET NAMES 'UTF8'");
+    //     $consulta = "SELECT * FROM contactos ORDER BY nombre, apellidos"; //definimos la query
+    //     $resultado = mysqli_query($conexion,$consulta); //ejecutamos la query
+    //    if(!$resultado){
+    //         throw new Exception("Error al ejecutar la consulta: " . mysqli_error($conexion));
+    //     }
+    //     while($fila = mysqli_fetch_array($resultado)){
+    //         echo "<p>$fila[cod_contacto]</p>";
+    //         echo "<p>$fila[nombre]</p>";
+    //         echo "<p>$fila[apellidos]</p>";
+    //     }
+    // }catch(Exception $e){
+    //     echo "<p>error: ".mysqli_errno($conexion)."</p>";
+    //     echo "<p>Mensaje: ".mysqli_error($conexion)."</p>";
+    // }
+    /*error_reporting(0) desactiva la notificacion de errores*/
+    /*error_reporting(E_ALL ^ E_WARNING/E_NOTICE) desactivar todo excepto tal o/tal */
+    // error_reporting(0);
+    try{
+        require("conexion_agenda.php");
+    
+    }catch(Throwable $t){
+        echo "<p>----------------------------------------</p>";
+        echo "Mensaje: ".$t->getMessage();
+        echo "<p>----------------------------------------</p>";
+        exit();
+    }
+
+    try{
+    // 1.- Conexión
+    $conexion = mysqli_connect($servidor,$usuario,$password,$bbdd);
+    mysqli_query($conexion,"SET NAMES 'UTF8'");
+    // 2.- Seleccionamos la BBDD
+    if(mysqli_select_db($conexion,$bbdd)){
+        // 3.- Definimos la query
+        $consulta = "SELECT * FROM contactos ORDER BY nombre, apellidos;";
+        // 4.- Ejecutamos la consulta
+        $resultado = mysqli_query($conexion, $consulta);
+        // 5.- Mostramos por pantalla (SELECT)
+        $contador=0;
+        while($fila=mysqli_fetch_array($resultado)){
+            // Declaramos la variable ruta que es la que buscará las imágenes que tenemos
+            $ruta="media/img/".$fila['foto'];
+            if($contador%2==0){
+                // echo "<section class='info-par'>";
+            }else{
+                // echo "<section class='info-impar'>";
             }
-            try{
-                // 1.- Conexión
-                $conexion = mysqli_connect($servidor,$usuario,$password,$bbdd);
-                mysqli_query($conexion,"SET NAMES 'UTF8'");
-                // 2.- Seleccionamos la BBDD
-                if(mysqli_select_db($conexion,$bbdd)){
-                    // 3.- Definimos la query
-                    $consulta = "SELECT * FROM contactos ORDER BY nombre, apellidos;";
-                    // 4.- Ejecutamos la consulta
-                    $resultado = mysqli_query($conexion, $consulta);
-                    // 5.- Mostramos por pantalla (SELECT)
-                    $contador=0;
-                    while($fila=mysqli_fetch_array($resultado)){
-                        $ruta="media/img/".$fila['foto'];
-                        if($contador%2==0){
-                            // echo "<section class='info-par'>";
-                        }else{
-                            // echo "<section class='info-impar'>";
-                        }
-                        if(file_exists($ruta)){
-                            echo "<img src='$ruta'>";
-                        }else{
-                            echo "<br>";
-                            echo "<img src='media/img/error.webp'>";
-                        }
-                        
-                        echo "<a href='mostrar_ficha.php?cod_contacto=$fila[cod_contacto]
-                        &nombre=$fila[nombre]
-                        &apellidos=$fila[apellidos]
-                        &mail=$fila[mail]
-                        &telefono=$fila[telefono]
-                        &observaciones=$fila[observaciones]
-                        &foto=$fila[foto]'>$fila[nombre] $fila[apellidos]</a>";
-                        echo "</section>";
-                        $contador++;
-                        /*
-                        echo "<p style='color:red'>$fila[cod_contacto]</p>";
-                        echo "<p>$fila[nombre]</p>";
-                        echo "<p>$fila[apellidos]</p>";
-                        */
-                    }
-                }
-                mysqli_close($conexion);
-            }catch(mysqli_sql_exception $mse){
-                //ha saltado una excepcion y vamos a mostrar error
-                echo "<p>Nº de error: ".$mse->getCode()."</p>";
-                echo "<p>Mensaje: ".$mse->getMessage()."</p>";
+            if(file_exists($ruta)){
+                echo "<img src='$ruta'>";
+            }else{
+                echo "<br>";
+                // Muestra una imagen de error cuando no se encuentra la foto del usuario
+                echo "<img src='media/img/error.webp'>";
             }
+
+            echo "<a href='mostrar_ficha.php?cod_contacto=$fila[cod_contacto]
+            &nombre=$fila[nombre]
+            &apellidos=$fila[apellidos]
+            &mail=$fila[mail]
+            &telefono=$fila[telefono]
+            &observaciones=$fila[observaciones]
+            &foto=$fila[foto]'>$fila[nombre] $fila[apellidos]</a>";
+            echo "</section>";
+            $contador++;
             /*
-            mostrar_contactos.php -> (dibujo de un lapiz) -> editar_contacto.php
-            UPDATE contactos SET
-            nombre = (una caja, o sa un input)
-            WHERE cod_contacto = (una caja, o sea un input)
+            echo "<p style='color:red'>$fila[cod_contacto]</p>";
+            echo "<p>$fila[nombre]</p>";
+            echo "<p>$fila[apellidos]</p>";
             */
-            ?>
+        }
+    }
+    mysqli_close($conexion);
+    }catch(mysqli_sql_exception $mse){
+        //ha saltado una excepcion y vamos a mostrar error
+        echo "<p>Nº de error: ".$mse->getCode()."</p>";
+        echo "<p>Mensaje: ".$mse->getMessage()."</p>";
+    }
+    /*
+    mostrar_contactos.php -> (dibujo de un lapiz) -> editar_contacto.php
+    UPDATE contactos SET
+    nombre = (una caja, o sa un input)
+    WHERE cod_contacto = (una caja, o sea un input)
+    */
+    ?>
 </body>
 </html>
